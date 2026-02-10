4a234999492dadcc7186e9f18f7eac49dc8d507c
BuildTools <javgarag2@gmail.com>
2026-02-10 20:19:29 +0100

    organize current code2

---
+++ base/lua/FPCameraPlayerBase.lua
@@ -0,0 +1,238 @@
+dofile(ModPath .. "lua/PlayerCamera.lua")
+
+UnitBase = UnitBase or class()
+FPCameraPlayerBase = FPCameraPlayerBase or class(UnitBase)
+
+local ids_left_ik_modifier_name = Idstring("left_arm_ik")
+local ids_right_ik_modifier_name = Idstring("right_arm_ik")
+
+function FPCameraPlayerBase:set_interaction_anim(interaction_anim)
+	self._interaction_anim = interaction_anim
+	return self._interaction_anim
+end
+
+function FPCameraPlayerBase:interaction_anim()
+	return self._interaction_anim
+end
+
+function FPCameraPlayerBase:clear_interaction_anim()
+	if self._interaction_anim then
+		self._unit:anim_state_machine():set_global(self._interaction_anim.weight, 0)
+		self:set_interaction_anim(nil)
+	end
+end
+
+-- Arm direction
+
+function FPCameraPlayerBase:set_interact_object(interact_object)
+	self._interact_object = interact_object
+end
+
+function FPCameraPlayerBase:clear_interact_object()
+	self._interact_object = nil
+end
+
+local orig_update_rot = FPCameraPlayerBase._update_rot
+function FPCameraPlayerBase:_update_rot(axis, unscaled_axis)
+	orig_update_rot(self, axis, unscaled_axis)
+	if not self._interact_object then return end
+
+	local interact_position = self._interact_object:interaction():interact_position()
+	local arm_position = self._unit:position()
+	local arm_rot = self._unit:rotation()
+	local relative = interact_position - arm_position
+	local interact_arm_offset_rot = Rotation()
+	local final_rot = Rotation()
+
+	mrotation.set_look_at(interact_arm_offset_rot, relative, math.UP)
+	mrotation.multiply(final_rot, interact_arm_offset_rot, arm_rot)
+
+	self:set_rotation(final_rot)
+end
+
+-- IK
+
+function FPCameraPlayerBase:set_ik_unit(ik_unit)
+	self._ik = ik_unit
+
+	self._ik_modifiers = {
+		left = self._unit:anim_state_machine():get_modifier(ids_left_ik_modifier_name),
+		right = self._unit:anim_state_machine():get_modifier(ids_right_ik_modifier_name)
+	}
+end
+
+function FPCameraPlayerBase:start_ik(start_left, start_right)
+	if start_left then
+		self._unit:anim_state_machine():force_modifier(ids_left_ik_modifier_name)
+	end
+
+	if start_right then
+		self._unit:anim_state_machine():force_modifier(ids_right_ik_modifier_name)
+	end
+end
+
+-- arm = "left" or "right"
+function FPCameraPlayerBase:update_ik(arm)
+	local locator = self._ik:get_object(Idstring("ik_" .. arm))
+	if not self._ik or not alive(self._ik) or not locator then
+		return
+	end
+
+	local pos = Vector3()
+	mvector3.add(pos, locator:local_position())
+	mvector3.rotate_with(pos, self._unit:rotation())
+	mvector3.add(pos, self._unit:position())
+
+	local rot = Rotation()
+	mrotation.multiply(rot, self._unit:rotation())
+	mrotation.multiply(rot, locator:local_rotation())
+
+	self:_update_ik_align(arm)
+
+	if not self._ik_modifiers[arm] then
+		log("[FPCameraPlayerBase:update_ik] Invalid arm for IK update: " .. tostring(arm))
+	end
+
+	self._ik_modifiers[arm]:set_target_position(pos)
+	self._ik_modifiers[arm]:set_target_rotation(rot)
+	--Application:draw_sphere(pos, 5, 1, 0, 0)
+end
+
+local pos = Vector3()
+local rot = Rotation()
+-- weapon is not parented to the hands so manually move it
+-- sadly this method does not allow for making the weapon spin with the anim 
+function FPCameraPlayerBase:_update_ik_align(arm)
+	local weap_unit = self._parent_unit:inventory():equipped_unit()
+	local align_obj = self._unit:get_object(Idstring("a_weapon_" .. arm))
+	local hand = self._unit:get_object(Idstring((arm == "right" and "Right" or "Left") .. "Hand"))
+
+	if not self._ik_align_offsets then
+		self._ik_align_offsets = {
+			pos = align_obj:position() - hand:position(),
+			rot = hand:rotation():inverse() * align_obj:rotation()
+		}
+
+		mvector3.rotate_with(self._ik_align_offsets.pos, hand:rotation():inverse())
+	end
+
+	mvector3.set_zero(pos)
+	mvector3.add(pos, self._ik_align_offsets.pos)
+	mvector3.rotate_with(pos, hand:rotation())
+	mvector3.add(pos, hand:position())
+
+	mrotation.set_zero(rot)
+	mrotation.multiply(rot, hand:rotation())
+	mrotation.multiply(rot, self._ik_align_offsets.rot)
+
+	weap_unit:set_position(pos)
+	weap_unit:set_rotation(rot)
+
+	--Application:draw_sphere(pos, 5, 0, 1, 0)
+end
+
+function FPCameraPlayerBase:stop_ik()
+	if not self._ik or not alive(self._ik) then
+		return
+	end
+
+	self._unit:anim_state_machine():forbid_modifier(ids_right_ik_modifier_name)
+	self._unit:anim_state_machine():forbid_modifier(ids_left_ik_modifier_name)
+
+	self._ik_align_offsets = nil
+end
+
+-- Avoid going into the empty state while doing IK or else it won't work
+local orig_anim_clbk_idle_full_blend = FPCameraPlayerBase.anim_clbk_idle_full_blend
+function FPCameraPlayerBase:anim_clbk_idle_full_blend()
+	if not self._ik:anim_data().playing then	
+		orig_anim_clbk_idle_full_blend(self)
+	end
+end
+
+-- Animation callbacks
+function FPCameraPlayerBase:spawn_interaction_items()
+	for _, unit in pairs((self._interaction_anim and self._interaction_anim.units) or {}) do
+		local aligns = unit.align_objects or {
+			"a_weapon_left"
+		}
+		self._interaction_item_units = self._interaction_item_units or {}
+		
+		for _, align in ipairs(aligns) do
+			local align_obj_name = Idstring(align)
+			local align_obj = self._unit:get_object(align_obj_name)
+			local spawned_unit = World:spawn_unit(Idstring(unit.unit_path), Vector3(), Rotation()) -- CRASH HERE? Interaction unit not loaded; did you forget to add it in main.xml?
+
+			spawned_unit:anim_stop()
+
+			if unit.material_config then
+				spawned_unit:set_material_config(Idstring(unit.material_config), true)
+			end
+
+			for i = 0, spawned_unit:num_bodies() - 1 do
+				spawned_unit:body(i):set_collisions_enabled(false)
+			end
+
+			self._unit:link(align_obj:name(), spawned_unit, spawned_unit:orientation_object():name())
+
+			if alive(spawned_unit) and spawned_unit:damage() and spawned_unit:damage():has_sequence(unit.unit_sequence) then
+				spawned_unit:damage():run_sequence_simple(unit.unit_sequence)
+			end
+
+			table.insert(self._interaction_item_units, spawned_unit)
+		end
+	end
+end
+
+function FPCameraPlayerBase:anim_clbk_unspawn_interaction_items()
+	if not self._interaction_item_units then
+		return
+	end
+
+	for _, unit in ipairs(self._interaction_item_units) do
+		if alive(unit) then
+			unit:unlink()
+			World:delete_unit(unit)
+		end
+	end
+
+	self._interaction_item_units = {}
+end
+
+-- Animation callbacks
+
+function FPCameraPlayerBase:anim_clbk_offhand_exit()
+	self:clear_interaction_anim()
+	self:clear_interact_object()
+end
+
+function FPCameraPlayerBase:anim_clbk_interact_hold_enter()
+	self.interaction_hold_anim_playing = true
+	self.queue_interaction_anim_exit = false
+end
+
+function FPCameraPlayerBase:anim_clbk_interact_hold_loop()
+	if self.queue_interaction_anim_exit then
+		self.queue_interaction_anim_exit = false
+		self:play_redirect(Idstring("interact_exit"))
+	end
+end
+
+function FPCameraPlayerBase:anim_clbk_interact_hold_exit()
+	self.interaction_hold_anim_playing = false
+end
+
+function FPCameraPlayerBase:anim_clbk_interact_interupt_enter()
+	self.interaction_interupt_anim_playing = true
+end
+
+function FPCameraPlayerBase:anim_clbk_interact_interupt_exit()
+	self.interaction_interupt_anim_playing = false
+
+	if self._interaction_anim.hide_weapon then
+		self:show_weapon()
+	end
+
+	self:clear_interaction_anim()
+	self:clear_interact_object()
+end
---
+++ base/lua/PlayerStandard.lua
@@ -0,0 +1,162 @@
+Hooks:PostHook(PlayerStandard, "_update_check_actions", "int_anim_update_check_interacting", function(self, t, dt, paused)
+    self:_update_unequip_interaction_timers(t)
+end)
+
+Hooks:PreHook(PlayerStandard, "_check_action_interact", "int_anim_do_interact_anim_checks", function(self, t, input)
+    self._interaction_unit = self._interaction:active_unit()
+    if not self._interaction_unit or not alive(self._interaction_unit) then
+        return
+    end
+
+    self._can_play_interact_anim = self._interaction_unit:interaction():can_interact(managers.player:player_unit())
+end)
+
+Hooks:PostHook(PlayerStandard, "_update_interaction_timers", "int_anim_update_interaction_timers", function(self, t)
+    if self._interaction_anim and self._interact_expire_t and ((self._interact_expire_t <= (self._interaction_anim.exit_when_time_left or 1))) and self._camera_unit:base().interaction_hold_anim_playing then
+        self._camera_unit:base().queue_interaction_anim_exit = true
+    end 
+end)
+
+--[[
+	PlayerStandard._ext_camera = PlayerCamera object
+	PlayerStandard._ext_camera:camera_unit() = FPCameraPlayerBase object
+]]--
+PlayerMovementState = PlayerMovementState or class()
+PlayerStandard = PlayerStandard or class(PlayerMovementState)
+
+-- Instant interactions
+function PlayerStandard:_play_interact_redirect(t)
+
+	if self._shooting or self._running or not self._equipped_unit:base():start_shooting_allowed() or self:_is_reloading() or self:_changing_weapon() or self:_is_meleeing() or self:in_steelsight() then
+		return
+	end
+
+	self._state_data.interact_redirect_t = t + 1
+	self._interaction_anim = self._camera_unit:base():set_interaction_anim(tweak_data.interaction.animations[self._interaction_unit:interaction().tweak_data])
+
+	local has_akimbo = alive(self._equipped_unit) and self._equipped_unit:base().akimbo
+
+	if not self._interaction_anim or not self._can_play_interact_anim or has_akimbo then
+		self._ext_camera:play_redirect(self:get_animation("use"))
+		return
+	end
+
+	self._ext_camera:play_redirect(Idstring(self._interaction_anim.animation_state_machine_name))
+	if self._interaction_anim.offhand_animation then
+		self._ext_camera:play_ik_redirect(self._interaction_anim.offhand_animation.name or "", self._interaction_anim.offhand_animation.speed or 1)
+	end
+end
+
+-- Timed interactions (hard overwrite)
+function PlayerStandard:_start_action_interact(t, input, timer, interact_object)
+	self:_interupt_action_reload(t)
+	self:_interupt_action_steelsight(t)
+	self:_interupt_action_running(t)
+	self:_interupt_action_charging_weapon(t)
+
+	local final_timer = timer
+	final_timer = managers.modifiers:modify_value("PlayerStandard:OnStartInteraction", final_timer, interact_object)
+	self._interact_expire_t = final_timer
+	local start_timer = 0
+	self._interact_params = {
+		object = interact_object,
+		timer = final_timer,
+		tweak_data = interact_object:interaction().tweak_data
+	}
+	
+	self._interaction_anim = self._camera_unit:base():set_interaction_anim(tweak_data.interaction.animations[self._interaction_unit:interaction().tweak_data])
+
+	if self._interaction_anim then
+		local tweak_data = self._equipped_unit:base():weapon_tweak_data()
+		self._unequip_weapon_on_interaction_expire_t = t + ((tweak_data.timers.unequip or 0.5) / (self._interaction_anim.unequip_speed_multiplier or 1))
+	end
+	self:_play_unequip_animation(self._interaction_anim and self._interaction_anim.unequip_speed_multiplier or nil)
+
+	managers.hud:show_interaction_bar(start_timer, final_timer)
+	managers.network:session():send_to_peers_synched("sync_teammate_progress", 1, true, self._interact_params.tweak_data, final_timer, false)
+	self._unit:network():send("sync_interaction_anim", true, self._interact_params.tweak_data)
+end
+
+-- Timed interaction interupt (play specific interupt redirect, also a hard overwrite)
+function PlayerStandard:_interupt_action_interact(t, input, complete)
+	if self._interact_expire_t then
+		self:_clear_tap_to_interact()
+
+		self._interact_expire_t = nil
+
+		if alive(self._interact_params.object) then
+			self._interact_params.object:interaction():interact_interupt(self._unit, complete)
+		end
+
+		self._ext_camera:camera_unit():base():remove_limits()
+		self._interaction:interupt_action_interact(self._unit)
+		managers.network:session():send_to_peers_synched("sync_teammate_progress", 1, false, self._interact_params.tweak_data, 0, complete and true or false)
+
+		self._interact_params = nil
+
+		if self._interaction_anim and self._camera_unit:base().interaction_hold_anim_playing then  -- Changes to vanilla code are here (+ see FPCameraPlayerBase:anim_clbk_interact_interupt_exit)
+			self._ext_camera:play_redirect(Idstring("interact_interupt"))
+		else
+			self._camera_unit:base():anim_clbk_unspawn_interaction_items()
+			self:_play_equip_animation()
+		end
+
+		managers.hud:hide_interaction_bar(complete)
+		self._unit:network():send("sync_interaction_anim", false, "") -- Vanilla third person animation
+	end
+end
+
+-- Equip/unequip hard overwrites
+function PlayerStandard:_play_equip_animation(speed_multiplier)
+	local tweak_data = self._equipped_unit:base():weapon_tweak_data()
+	self._equip_weapon_expire_t = managers.player:player_timer():time() + (tweak_data.timers.equip or 0.7)
+	self._ext_camera:play_redirect(self:get_animation("equip"), speed_multiplier or nil)
+
+	self._equipped_unit:base():tweak_data_anim_stop("unequip")
+	self._equipped_unit:base():tweak_data_anim_play("equip", speed_multiplier or nil)
+
+	local interaction_anim = self._camera_unit:base():interaction_anim()
+	if interaction_anim then
+		if interaction_anim.hide_weapon then
+			self._camera_unit:base():show_weapon()
+		end
+		self._camera_unit:base():clear_interaction_anim()
+		self._camera_unit:base():clear_interact_object()
+	end
+end
+
+function PlayerStandard:_play_unequip_animation(speed_multiplier)
+	self._ext_camera:play_redirect(self:get_animation("unequip"), speed_multiplier or nil)
+	self._equipped_unit:base():tweak_data_anim_stop("equip")
+	self._equipped_unit:base():tweak_data_anim_play("unequip", speed_multiplier or nil)
+end
+
+-- For PlayerCarry; cancelling an interaction causes bag drop as interaction has technically "ended" but self:_changing_weapons() returns false (as interaction interupt anim is playing).
+-- More observations: self:_interacting() will always return false in PlayerCarry:_check_use_item() when interupting an interaction.
+function PlayerStandard:_changing_weapon()
+	return self._unequip_weapon_expire_t or self._equip_weapon_expire_t or self._camera_unit:base().interaction_interupt_anim_playing
+end
+
+-- Called every frame to check if the unequip interaction timer has expired
+function PlayerStandard:_update_unequip_interaction_timers(t)
+	if self._camera_unit:base():interaction_anim() and self._unequip_weapon_on_interaction_expire_t and self._unequip_weapon_on_interaction_expire_t <= t then
+		self._unequip_weapon_on_interaction_expire_t = nil
+
+		if self._shooting or self._running or not self._equipped_unit:base():start_shooting_allowed() or self:_is_reloading() or self:_changing_weapon() or self:_is_meleeing() or self:in_steelsight() then
+			return
+		end
+
+		self._camera_unit:base().has_unequipped_for_interaction = true
+
+		if self._interact_params and self._interact_params.timer > 0 then
+			self._camera_unit:base():set_interact_object(self._interact_params.object)
+			self._ext_camera:play_redirect(Idstring(self._interaction_anim.animation_state_machine_name))
+
+			if self._interaction_anim.hide_weapon then
+				self._camera_unit:base():hide_weapon()
+			end
+		else
+			self._ext_camera:play_redirect(Idstring("interact"))
+		end
+	end
+end
